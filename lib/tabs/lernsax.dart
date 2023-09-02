3124cc7a2ead842d8f1150f7de304da36fdbdf2b
AntonioAlbt <148400502+AntonioAlbt@users.noreply.github.com>
2023-09-02 17:46:19 +0200

    Fix lernsax auth (it works now yay :D)

--- lib/tabs/lernsax.dart
+++ lib/tabs/lernsax.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:kepler_app/libs/state.dart';
+import 'package:provider/provider.dart';
 
 class LernSaxTab extends StatefulWidget {
   const LernSaxTab({super.key});
@@ -10,11 +12,14 @@ class LernSaxTab extends StatefulWidget {
 class _LernSaxTabState extends State<LernSaxTab> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.abc), label: "LernSax 1"),
-        BottomNavigationBarItem(icon: Icon(Icons.ac_unit), label: "Lernsax 2")
-      ]),
+    return Center(
+      child: Consumer<CredentialStore>(
+        builder: (context, creds, _) {
+          return Text(
+            "${creds.lernSaxLogin}\n\nHochsicheres Token!!! -> ${creds.lernSaxToken}"
+          );
+        }
+      ),
     );
   }
 }
