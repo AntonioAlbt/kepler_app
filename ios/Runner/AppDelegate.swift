792bae2a558af044884b323f45c84fde9e30a8ce
AntonioAlbt <148400502+AntonioAlbt@users.noreply.github.com>
2026-05-08 01:41:26 +0200

    New AppDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,7 +2,7 @@ import UIKit
 import Flutter
 import workmanager
 
-@UIApplicationMain
+@main // This replaces @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
@@ -10,18 +10,14 @@ import workmanager
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
 
-    WorkmanagerPlugin.setPluginRegistrantCallback { registry in  
-      // The following code will be called upon WorkmanagerPlugin's registration.
-      // Note : all of the app's plugins may not be required in this context ;
-      // instead of using GeneratedPluginRegistrant.register(with: registry),
-      // you may want to register only specific plugins.
-      GeneratedPluginRegistrant.register(with: registry)
+    WorkmanagerPlugin.setPluginRegistrantCallback { registry in
+        GeneratedPluginRegistrant.register(with: registry)
     }
 
-    // same duration as on android, in seconds
+    // Set background fetch interval (18000 seconds / 5 hours)
     UIApplication.shared.setMinimumBackgroundFetchInterval(TimeInterval(300*60))
 
-    // for flutter_local_notifications
+    // For flutter_local_notifications
     if #available(iOS 10.0, *) {
       UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
     }
